3520cd777405cbbc7ee0481c928f60c64975cf32
bartisricsi <Milkatehen88>
2020-12-09 16:32:35 +0200

    recyclerview

---
+++ app/src/main/java/com/example/android2020/Adapter.kt
@@ -0,0 +1,30 @@
+package com.example.android2020
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item.view.*
+
+class Adapter(private val exampleList: List<ExampleItem>) :
+        RecyclerView.Adapter<Adapter.ExampleViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item,
+                parent, false)
+        return ExampleViewHolder(itemView)
+    }
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+        val currentItem = exampleList[position]
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
+    }
+    override fun getItemCount() = exampleList.size
+    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.image_view
+        val textView1: TextView = itemView.text_view_1
+        val textView2: TextView = itemView.text_view_2
+    }
+}
---
+++ app/src/main/java/com/example/android2020/ExampleItem.kt
@@ -0,0 +1,4 @@
+package com.example.android2020
+
+data class ExampleItem(val imageResource: Int, val text1: String, val text2: String) {
+}
